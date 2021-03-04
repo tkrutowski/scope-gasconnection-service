ba522065084dfda98772615cc92b5e7481f30cc0
Tomasz Krutowski <tkrutowski@gmail.com>
2021-03-04 22:08:31 +0100

    init

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+ INSERT INTO Zakres_przylacze(id_zadania, rodzaj_zadania,inne,srednica,dlugosc,material,sdr,dlugosc_proj,szafke_dostarcza,szafka_data_odbioru)
+ values (1,'przylacze','jakas wiadopmosc',25,11.4,'PE100RC','SDF 11', 12.5,'Klient', '2020-08-05');
+
