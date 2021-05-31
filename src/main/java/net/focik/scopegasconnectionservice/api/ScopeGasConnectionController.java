b7ed915d96c35e7a6c65314c2033f581c3d64378
Tomasz Krutowski <tkrutowski@gmail.com>
2021-05-31 10:21:18 +0200

    add log into controller

--- src/main/java/net/focik/scopegasconnectionservice/api/ScopeGasConnectionController.java
+++ src/main/java/net/focik/scopegasconnectionservice/api/ScopeGasConnectionController.java
@@ -1,6 +1,7 @@
 package net.focik.scopegasconnectionservice.api;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import net.focik.scopegasconnectionservice.domain.ScopeGasConnectionFacade;
 import net.focik.scopegasconnectionservice.domain.dto.ScopeGasConnectionDto;
 import net.focik.scopegasconnectionservice.domain.share.TaskType;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Log4j2
 @RestController
 @AllArgsConstructor
 @RequestMapping("/api/scopegasconnection")
@@ -31,7 +33,10 @@ class ScopeGasConnectionController {
     @GetMapping("/task/{idTask}")
     ResponseEntity<List<ScopeGasConnectionDto>> getScopeGasConnection(@PathVariable Integer idTask, @RequestParam(name = "type", defaultValue = "GAS_CONNECTION") TaskType taskType) {
         int i = 0;
+        log.info("SCOPE-GASCONNECTION-SERVICE: Try find scope-gasconnection for  id = " + idTask + "and type: "+taskType);
         List<ScopeGasConnectionDto> gasConnectionDtoList = scopeGasConnectionFacade.getScopeGasConnectionDtoList(idTask, taskType);
+        log.info("SCOPE-GASCONNECTION-SERVICE: Found "+gasConnectionDtoList.size() +" scope-gasconnection for id = " + idTask + " and task type: " + taskType);
+
         return new ResponseEntity<>(gasConnectionDtoList, HttpStatus.OK);
     }
 
